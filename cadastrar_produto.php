37fc8a4d613a60be18d0d269c664f6f8a10571e2
loft <kaua1101rrc@gmail.com>
2026-03-11 10:53:45 -0300

    Back de cadastro de produto realizado.

--- cadastrar_produto.php
+++ cadastrar_produto.php
@@ -1,6 +1,43 @@
 <?php
     require_once 'template/header.php';
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $nome_produto = $_POST['nome_produto'];
+    $preco = $_POST['preco'];
+
+    //Verifica se as variáveis estão vazias
+    /* if(!empty($nome_produto) && !empty($preco)) {
+        
+    } */
+
+    if(empty($nome_produto)) {
+        echo "Preencha o nome do produto";
+    } elseif(empty($preco)) {
+        echo "Preencha o preço do produto";
+    } else {
+        //SQL
+        $sql = 'INSERT INTO produtos (nome_produto, preco) VALUES (:nome_produto, :preco)';
+
+        $stmt = $pdo->prepare($sql);
+
+        $stmt->bindParam(':nome_produto', $nome_produto);
+        $stmt->bindParam(':preco', $preco);
+
+        $stmt->execute();
+
+        echo "Produto cadastrado com sucesso!";
+    }
+}
+    
 ?>
+
+<form class="cadastro_de_produto" action="" method="POST">
+    <label class="label_camposCadastroProduto">Nome do produto: </label>
+    <input class="campoCadastroProduto" type="text" name="nome_produto" placeholder="Insira o nome do produto..."> 
+    <label class="label_camposCadastroProduto">Preço:</label>
+    <input class="campoCadastroProduto" type="number" name="preco" step="0.01" placeholder="Insira o preço do produto...">
+    <input class="botaoCadastroProduto" type="submit" value="Cadastrar">
+</form>
 
 <?php 
     require_once 'template/footer.php';
