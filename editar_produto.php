41d47fa2e94aee372d7d97fbf9706f88f5926bc9
loft <kaua1101rrc@gmail.com>
2026-03-13 15:28:03 -0300

    sql de editar o produto

--- editar_produto.php
+++ editar_produto.php
@@ -1,6 +1,16 @@
 <?php
     include_once("helpers/connection.php");
     include_once("template/header.php");
+
+    $sqlEditarNome = 'UPDATE produtos SET nome = :nome WHERE id = :id';
+    $sqlEditarPreco = 'UPDATE produtos SET preco = :preco WHERE id = :id';
+    
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':id', $produto_id);
+    $stmt->bindParam(':nome', $nome_produto);
+    $stmt->bindParam(':preco', $preco);
+    $stmt->execute();
+
 ?>
 
 <?php 
