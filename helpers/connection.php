c96311d41d1581de1156c9e167d4447c2c710b22
Kauã <kaua1101rrc@gmail.com>
2026-03-17 22:13:33 -0300

    alguns ajustes

--- helpers/connection.php
+++ helpers/connection.php
@@ -6,7 +6,7 @@ $dsn = "mysql:host=$ENV_HOST;dbname=$ENV_DBNAME";
 
 try {
     $pdo = new PDO($dsn, $ENV_USERNAME, $ENV_PASSWORD);
-    echo "Conexão com o banco bem sucedida! <hr> <br>";
+    /* echo "Conexão com o banco bem sucedida! <hr> <br>"; */
 } catch (PDOException $e) { // PDOException é uma mensagem de erro que guardei na variavel $e
     echo 'Erro na conexão: ' . $e->getMessage(); //getMessage traz a mensagem de erro
 }
