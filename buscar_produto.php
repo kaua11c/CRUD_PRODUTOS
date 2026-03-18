dc03b91d24287f2e0bd8a44bb52c13f55954dbe0
Kauã <kaua1101rrc@gmail.com>
2026-03-17 22:33:00 -0300

    melhorando projeto

---
+++ buscar_produto.php
@@ -0,0 +1,11 @@
+<?php
+    include_once("helpers/connection.php");
+    include_once("template/header.php");
+
+    $sql = "SELECT FROM produtos WHERE :nome_produto"
+
+?>
+
+<?php 
+    include_once("template/footer.php");
+?>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+    include_once("helpers/connection.php");
+    include_once("template/header.php");
+
+?>
+
+<div class="inicioIndex">
+    <h1>Bem-vindo!</h1>
+</div>
+
+<?php 
+    include_once("template/footer.php");
+?>
