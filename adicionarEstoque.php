31d486acd4d557ea328081c2395f4913ec8828ea
Kauã <kaua1101rrc@gmail.com>
2026-04-13 22:08:38 -0300

    a

--- adicionarEstoque.php
+++ adicionarEstoque.php
@@ -3,10 +3,32 @@
 include_once("helpers/connection.php");
 include_once("template/header.php");
 
-$sql = "UPDATE produtos SET estoque = estoque + :adicionarEstoque WHERE id = :id";
-$stmt = $pdo->prepare($sql);
-$stmt->bindParam(':adicionarEstoque', $adicionarEstoque);
-$stmt->bindParam(':id', $id);
-$stmt->execute();
+//Armazena o ID da URL na váriael $id
+$id = $_GET['id'] ?? null;
+
+//Valida se possuí um array válido
+if ($id) {
+    //CRUD PARA ARMAZENAR O $produto em um array
+    $sql = "SELECT * FROM produtos WHERE id = :id";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':id', $id);
+    $stmt->execute();
+
+    $produto = $stmt->fetch(PDO::FETCH_ASSOC);
+}
+
+if($_SERVER['REQUEST_METHOD'] === 'POST') { 
+    $id = $_POST['id'] ?? null;
+    $adionarEstoque = $_POST['adicionarEstoque'] ?? null;
+
+    $sql = "UPDATE produtos SET estoque = estoque + :adicionarEstoque WHERE id = :id";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':adicionarEstoque', $adicionarEstoque);
+    $stmt->bindParam(':id', $id);
+    $stmt->execute(); 
+}
+
 
 ?>
+
+<form action=""></form>
